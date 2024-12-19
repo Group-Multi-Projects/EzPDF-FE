fa9529e1ee281b994c6b958533bd72961ceac0de
Admin <tutquan456@gmail.com>
2024-12-19 21:57:12 +0700

    add home page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,20 +1,58 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Layout from "@/component/layout/layout";
 import { AddShape } from "@/component/addShape/addShape";
 import Login from "@/page/login";
 import PDFEditor from "@/page/edit/pdfEditor";
+import HomePage from "@/page/home/home";
+
 import Signup from "@/page/signup";
 
-const AppRouter = () =>{
-    return(
+const AppRouter = () => {
+  return (
     <Router>
-        <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/addShape" element={<AddShape />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/editPage" element={<PDFEditor/>} />
+      <Routes>
+        {/* Routes không có Layout */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        
+        {/* Routes có Layout */}
+        <Route 
+          path="/" 
+          element={
+            <Layout>
+              <Navigate to="/addShape" />
+            </Layout>
+          } 
+        />
+        <Route 
+          path="/addShape" 
+          element={
+            <Layout>
+              <AddShape />
+            </Layout>
+          } 
+        />
+        <Route 
+          path="/editPage" 
+          element={
+            <Layout>
+              <PDFEditor />
+            </Layout>
+          } 
+        />
+
+        <Route 
+          path="/home" 
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          } 
+        />
       </Routes>
+      
     </Router>
-    )
-}
-export default AppRouter
+  );
+};
+
+export default AppRouter;
